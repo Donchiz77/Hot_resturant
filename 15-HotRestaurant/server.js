5fa9cc2b2c5c71f89add3e061a280ef4d74dc014
Don Chisolm <don.chisolm14@gmail.com>
2021-07-08 20:32:46 -0400

    script updated to display tables and reservations

--- 15-HotRestaurant/server.js
+++ 15-HotRestaurant/server.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const path = require('path');
 
@@ -9,12 +8,12 @@ const PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
 const tables = [];
+app.get('/tables', (req, res) => res.sendFile(path.join(__dirname, 'tables.html')));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'home.html')));
 
-app.get('/add', (req, res) => res.sendFile(path.join(__dirname, 'reserve.html')));
+app.get('/reserve', (req, res) => res.sendFile(path.join(__dirname, 'reserve.html')));
 
 app.get('/api/tables', (req, res) => res.json(tables));
 
